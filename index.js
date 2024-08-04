b3faee22e1d53eecd9d39821c8b9722efce493e2
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-08-04 18:08:45 -0300

    actualización de puerto

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require("./src/server");
 const {database} = require("./src/db")
-const PORT = 3001;
 
+const port = process.env.PORT || 4000;
 
 database.sync({alter: true}).then(()=>{
-    server.listen(PORT, ()=>{
-        console.log("Listening in port", PORT);
+    server.listen(port, ()=>{
+        console.log("Listening in port", port);
     });
 });
 
