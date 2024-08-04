a1cff0cc8e49aef00f4f9fe542a8afdf11ab44aa
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-08-04 18:01:09 -0300

    agrege url de deploy en db

--- src/db.js
+++ src/db.js
@@ -1,15 +1,18 @@
 require("dotenv").config()
 
 const {Sequelize, DataTypes} = require("sequelize");
-const { USER, PASSWORD, HOST, PORT, BDD} = process.env;
+const { USER, PASSWORD, HOST, PORT, BDD, DATA_BASE_URL} = process.env;
 const CharacterFunction = require("./models/Characters")
 const EpisodeFunction = require("./models/Episodes")
 
 
-const database = new Sequelize(`postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${BDD}`,
+const database = new Sequelize(DATA_BASE_URL,
     {logging: false}
 )
-
+/* const database = new Sequelize(`postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${BDD}`,
+    {logging: false}
+) */
+   
 CharacterFunction(database,DataTypes)
 EpisodeFunction(database,DataTypes)
 
